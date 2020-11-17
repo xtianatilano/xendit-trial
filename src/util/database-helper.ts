cce598ec57804dfcdcc47faf407443554e82c47c
Christian Jerson Atilano <xtian.atilano@gmail.com>
2020-11-17 14:19:01 +0800

    added return type

--- src/util/database-helper.ts
+++ src/util/database-helper.ts
@@ -2,7 +2,7 @@
  * Utility helper to generate dynamic count of values for insert
  * @param data
  */
-export const generateValuePlaceholder = (data: any) => {
+export const generateValuePlaceholder = (data: any): string => {
     const valuePlaceholder = Object.keys(data).map((_value, index) => `$${index + 1}`).join(', ');
     return valuePlaceholder
 }
@@ -11,7 +11,7 @@ export const generateValuePlaceholder = (data: any) => {
  * Utility helper to generate insert columns from camelCase to snake_case
  * @param data
  */
-export const generateInsertPlaceholder = (data: any) => {
+export const generateInsertPlaceholder = (data: any): string => {
     const insertPlaceholder = Object.keys(data).map(value => value.replace( /([A-Z])/g, "_$1").toLowerCase()).join(', ');
     return insertPlaceholder;
 }
