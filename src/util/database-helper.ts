a6c739fb7345e7853d7dee3f262ce3e132406c9a
Christian Jerson Atilano <xtian.atilano@gmail.com>
2020-11-17 14:18:20 +0800

    fix missing return

--- src/util/database-helper.ts
+++ src/util/database-helper.ts
@@ -1,7 +1,17 @@
+/**
+ * Utility helper to generate dynamic count of values for insert
+ * @param data
+ */
 export const generateValuePlaceholder = (data: any) => {
     const valuePlaceholder = Object.keys(data).map((_value, index) => `$${index + 1}`).join(', ');
+    return valuePlaceholder
 }
 
+/**
+ * Utility helper to generate insert columns from camelCase to snake_case
+ * @param data
+ */
 export const generateInsertPlaceholder = (data: any) => {
     const insertPlaceholder = Object.keys(data).map(value => value.replace( /([A-Z])/g, "_$1").toLowerCase()).join(', ');
+    return insertPlaceholder;
 }
